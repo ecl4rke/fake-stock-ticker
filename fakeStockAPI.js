ce5b57b7aac54836d3f87fecf53e294ce1b9ba76
ecl4rke <teamflashonline@gmail.com>
2026-06-02 17:38:00 -0400

    cleaning up code
    removed unnecessary comments from code

--- fakeStockAPI.js
+++ fakeStockAPI.js
@@ -2,10 +2,9 @@ export function getStockData() {
     return {
         name: 'EpicVues',
         sym: 'EpV',
-        /* return a random number between 0 and 3 to two decimal places */
         price: (Math.random()*3).toFixed(2),  
 
-        /* return a timestamp  in this format: hh/mm/ss */
+        /* returns a timestamp the format: hh/mm/ss */
         time: new Date().toTimeString().slice(0, 8)
     }
 }
