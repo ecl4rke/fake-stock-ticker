1d53ff9d2435d0f3282eb98839b4dbc5fa2444f8
ecl4rke <teamflashonline@gmail.com>
2026-06-03 00:38:55 -0400

    cleaned up code

--- fakeStockAPI.js
+++ fakeStockAPI.js
@@ -2,7 +2,7 @@ export function getStockData() {
     return {
         name: 'EpicVues',
         sym: 'EpV',
-        price: (Math.random()*3).toFixed(2),  
+        price: Number((Math.random()*3).toFixed(2)),  
 
         /* returns a timestamp the format: hh/mm/ss */
         time: new Date().toTimeString().slice(0, 8)
--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import {getStockData} from "./fakeStockAPI.js"
 
+const stockDisplayName = document.getElementById("name")
+const stockDisplaySymbol = document.getElementById("symbol")
+const stockDisplayPrice = document.getElementById("price")
+const stockDisplayPriceIcon = document.getElementById("price-icon")
+const stockDisplayTime = document.getElementById("time")
+
 let prevPrice = null
 
 function renderStockTicker(stockData) {
-  const stockDisplayName = document.getElementById("name")
-  const stockDisplaySymbol = document.getElementById("symbol")
-  const stockDisplayPrice = document.getElementById("price")
-  const stockDisplayPriceIcon = document.getElementById("price-icon")
-  const stockDisplayTime = document.getElementById("time")
-
   const {name, sym, price, time} = stockData
 
   stockDisplayName.textContent = `Name: ${name}`
   stockDisplaySymbol.textContent = `Symbol: ${sym}`
   stockDisplayPrice.textContent = `Price: ${price}`
-  stockDisplayPriceIcon.innerHTML = price < prevPrice ? `<div class="triangle down">`
-                                      : price > prevPrice ? `<div class="triangle up">`
-                                      : `<div class="triangle right">`
+  stockDisplayPriceIcon.innerHTML = price === null ? `<div class="triangle right"></div>`
+                                      : price < prevPrice ? `<div class="triangle down"></div>`
+                                      : price > prevPrice ? `<div class="triangle up"></div>`
+                                      : `<div class="triangle right"></div>`
   
   stockDisplayTime.textContent = `Time: ${time}`
   prevPrice = price
 }
 
 renderStockTicker(getStockData())
-const intervalRender = setInterval(() => renderStockTicker(getStockData()), 1000)
+const intervalRender = setInterval(() => renderStockTicker(getStockData()), 1500)
